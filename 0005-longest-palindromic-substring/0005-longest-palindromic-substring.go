e5774167ae26f23768956da2786406143c5f3c64
Prajakta patil <prajaktavijaypatil02@gmail.com>
2026-07-09 20:51:39 +0530

    Time: 3 ms (56.95%), Space: 4.3 MB (99.94%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
@@ -0,0 +1,39 @@
+func longestPalindrome(s string) string {
+	if len(s) < 2 {
+		return s
+	}
+
+	start, end := 0, 0
+
+	for i := 0; i < len(s); i++ {
+		// Odd length palindrome
+		len1 := expandAroundCenter(s, i, i)
+
+		// Even length palindrome
+		len2 := expandAroundCenter(s, i, i+1)
+
+		maxLen := max(len1, len2)
+
+		if maxLen > end-start+1 {
+			start = i - (maxLen-1)/2
+			end = i + maxLen/2
+		}
+	}
+
+	return s[start : end+1]
+}
+
+func expandAroundCenter(s string, left, right int) int {
+	for left >= 0 && right < len(s) && s[left] == s[right] {
+		left--
+		right++
+	}
+	return right - left - 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
