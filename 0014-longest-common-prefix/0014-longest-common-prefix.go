db164525791e21c87c6310444cd7a76a8db862de
Prajakta patil <prajaktavijaypatil02@gmail.com>
2026-07-10 20:20:52 +0530

    Time: 0 ms (100%), Space: 4.2 MB (57.31%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.go
@@ -0,0 +1,18 @@
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	prefix := strs[0]
+
+	for _, str := range strs[1:] {
+		for len(prefix) > len(str) || str[:len(prefix)] != prefix {
+			prefix = prefix[:len(prefix)-1]
+			if prefix == "" {
+				return ""
+			}
+		}
+	}
+
+	return prefix
+}
