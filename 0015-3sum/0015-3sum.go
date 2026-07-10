d2724866fa409e2a0efbfb0d504e6323d9b02f8f
Prajakta patil <prajaktavijaypatil02@gmail.com>
2026-07-10 20:21:39 +0530

    Time: 24 ms (48.88%), Space: 9.7 MB (52.51%) - LeetHub

---
+++ 0015-3sum/0015-3sum.go
@@ -0,0 +1,46 @@
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+
+	var result [][]int
+	n := len(nums)
+
+	for i := 0; i < n-2; i++ {
+
+		// Skip duplicate first elements
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		left, right := i+1, n-1
+
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+
+			if sum == 0 {
+				result = append(result, []int{nums[i], nums[left], nums[right]})
+
+				left++
+				right--
+
+				// Skip duplicate left values
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+
+				// Skip duplicate right values
+				for left < right && nums[right] == nums[right+1] {
+					right--
+				}
+
+			} else if sum < 0 {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+
+	return result
+}
