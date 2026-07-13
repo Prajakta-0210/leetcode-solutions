69ab5b7ddc5e82ef5e7b84d4c19765b66575ab0f
Prajakta patil <prajaktavijaypatil02@gmail.com>
2026-07-13 22:22:38 +0530

    Time: 0 ms (100%), Space: 5.4 MB (100%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.go
@@ -0,0 +1,42 @@
+import "sort"
+
+func permuteUnique(nums []int) [][]int {
+	sort.Ints(nums)
+
+	var result [][]int
+	var path []int
+	used := make([]bool, len(nums))
+
+	var backtrack func()
+	backtrack = func() {
+		if len(path) == len(nums) {
+			temp := make([]int, len(path))
+			copy(temp, path)
+			result = append(result, temp)
+			return
+		}
+
+		for i := 0; i < len(nums); i++ {
+			
+			if used[i] {
+				continue
+			}
+
+			
+			if i > 0 && nums[i] == nums[i-1] && !used[i-1] {
+				continue
+			}
+
+			used[i] = true
+			path = append(path, nums[i])
+
+			backtrack()
+
+			path = path[:len(path)-1]
+			used[i] = false
+		}
+	}
+
+	backtrack()
+	return result
+}
