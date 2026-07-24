20c8f747265b3b56e4293201da7efdc4c8023c4c
Prajakta patil <prajaktavijaypatil02@gmail.com>
2026-07-24 22:25:15 +0530

    Time: 0 ms (100%), Space: 6 MB (47.11%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.go
@@ -0,0 +1,32 @@
+func insert(intervals [][]int, newInterval []int) [][]int {
+    result := [][]int{}
+    i := 0
+    n := len(intervals)
+
+    // 1. Add all intervals before newInterval
+    for i < n && intervals[i][1] < newInterval[0] {
+        result = append(result, intervals[i])
+        i++
+    }
+
+    // 2. Merge overlapping intervals
+    for i < n && intervals[i][0] <= newInterval[1] {
+        if intervals[i][0] < newInterval[0] {
+            newInterval[0] = intervals[i][0]
+        }
+        if intervals[i][1] > newInterval[1] {
+            newInterval[1] = intervals[i][1]
+        }
+        i++
+    }
+
+    result = append(result, newInterval)
+
+    // 3. Add remaining intervals
+    for i < n {
+        result = append(result, intervals[i])
+        i++
+    }
+
+    return result
+}
