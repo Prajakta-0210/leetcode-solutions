c9edc4c709ea44112021518b098ed512386ca637
Prajakta patil <prajaktavijaypatil02@gmail.com>
2026-07-28 19:27:22 +0530

    Time: 0 ms (100%), Space: 4.1 MB (60.49%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.go
@@ -0,0 +1,22 @@
+func mySqrt(x int) int {
+	if x < 2 {
+		return x
+	}
+
+	left, right := 1, x/2
+	ans := 0
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		// Avoid overflow by comparing mid <= x/mid
+		if mid <= x/mid {
+			ans = mid
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return ans
+}
