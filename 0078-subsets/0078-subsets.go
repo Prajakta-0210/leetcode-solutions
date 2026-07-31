76cfe9428f9cf0ff653535070d4bdbc80753605a
Prajakta patil <prajaktavijaypatil02@gmail.com>
2026-07-31 15:40:53 +0530

    Time: 0 ms (100%), Space: 4.1 MB (81.88%) - LeetHub

---
+++ 0078-subsets/0078-subsets.go
@@ -0,0 +1,24 @@
+func subsets(nums []int) [][]int {
+	result := [][]int{}
+	current := []int{}
+
+	var backtrack func(start int)
+
+	backtrack = func(start int) {
+		subset := make([]int, len(current))
+		copy(subset, current)
+		result = append(result, subset)
+
+		for i := start; i < len(nums); i++ {
+			current = append(current, nums[i])
+
+			backtrack(i + 1)
+
+			current = current[:len(current)-1]
+		}
+	}
+
+	backtrack(0)
+
+	return result
+}
